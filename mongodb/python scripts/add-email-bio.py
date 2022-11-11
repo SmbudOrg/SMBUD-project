ff8529b0491d4ae08b212612d56e65e46e42be44
SaraMrng <90102746+SaraMrng@users.noreply.github.com>
2022-11-11 11:34:32 +0100

    Update add-email-bio.py

--- mongodb/python scripts/add-email-bio.py
+++ mongodb/python scripts/add-email-bio.py
@@ -29,18 +29,18 @@ universities = [
 with open(inputfile, "r", encoding="utf-8") as input:
     data = json.load(input)
 
-    for i in range(0, len(data)):
-        for j in range(0, len(data[i]["authors"])):
-            name = data[i]["authors"][j]["name"]
+for i in range(0, len(data)):
+    for j in range(0, len(data[i]["authors"])):
+        name = data[i]["authors"][j]["name"]
 
-            if (random.random() <= emailp):
-                emailname = (name.replace(" ", "")).lower()
-                data[i]["authors"][j]["email"] = emailname + "@gmail.com"
-            
-            if (random.random() <= biop):
-                u = random.randint(0, len(universities)-1)
-                bio = name + " was born in " + str(random.randint(1940, 2000)) + " and studied at " + universities[u]
-                data[i]["authors"][j]["bio"] = bio
+        if (random.random() <= emailp):
+            emailname = (name.replace(" ", "")).lower()
+            data[i]["authors"][j]["email"] = emailname + "@gmail.com"
+
+        if (random.random() <= biop):
+            u = random.randint(0, len(universities)-1)
+            bio = name + " was born in " + str(random.randint(1940, 2000)) + " and studied at " + universities[u]
+            data[i]["authors"][j]["bio"] = bio
             
 
 with open(outputfile, "w", encoding="utf-8") as output:
