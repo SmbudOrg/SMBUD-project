a92817bf2ac765133c1ba723e7aa7d01c50d9f79
Pietro Caforio <34812011+Pete2000@users.noreply.github.com>
2022-11-14 23:57:22 +0100

    Create add_references.py

---
+++ mongodb/python scripts/add_references.py
@@ -0,0 +1,14 @@
+import json
+import random
+import sys
+
+inputfile = "db_conTesti.json"
+outputfile = "dataset_conrefs.json"
+
+data = json.load(open(inputfile,"r",encoding="utf-8"))
+
+for i in range(0,len(data)):
+    data[i]["references"] = {}
+    data[i]["references"] = [data[i]["_id"] for i in range(0,random.randint(0,10)) ]
+
+json.dump(data, open(outputfile,"w",encoding="utf-8"), indent = 4)
