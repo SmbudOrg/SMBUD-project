731932dd9f387ca56ec641903d716032f119d571
Pietro Caforio <34812011+Pete2000@users.noreply.github.com>
2022-11-15 17:20:09 +0100

    Update add_references.py

--- mongodb/python scripts/add_references.py
+++ mongodb/python scripts/add_references.py
@@ -9,6 +9,6 @@ data = json.load(open(inputfile,"r",encoding="utf-8"))
 
 for i in range(0,len(data)):
     data[i]["references"] = {}
-    data[i]["references"] = [data[i]["_id"] for i in range(0,random.randint(0,10)) ]
+    data[i]["references"] = [data[random.randint(0,len(data)-1)]["_id"] for i in range(0,random.randint(0,10))]
 
 json.dump(data, open(outputfile,"w",encoding="utf-8"), indent = 4)
