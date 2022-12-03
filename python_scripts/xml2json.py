53b94738f71c63c0160c37fc329628a3102960b7
Pietro Caforio <34812011+Pete2000@users.noreply.github.com>
2022-12-03 14:54:18 +0100

    Update xml2json.py

--- python_scripts/xml2json.py
+++ python_scripts/xml2json.py
@@ -1,5 +1,11 @@
-import xmltodict,gzip,json
-xml = gzip.GzipFile("dblp.xml.gz")
-f = open("dblp.json","w")
-f.write(json.dumps(xmltodict.parse(xml), indent=4))
+import xmltodict,json,os
+
+folderpath = "dataset/"
+inputname = "dataset/book-db.xml"
+outputname ="dataset/book-db.json"
+for filename in os.listdir(folderpath):
+    xml = open(folderpath+filename,"rb")
+    f = open(filename.replace("xml","json"),"w")
+    f.write(json.dumps(xmltodict.parse(xml), indent=4))
+
 
