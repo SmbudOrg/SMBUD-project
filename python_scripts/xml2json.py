5eb9fe08d28c5a1d9a146aa192690ac488aa7fd8
Pietro Caforio <34812011+Pete2000@users.noreply.github.com>
2022-12-03 14:55:26 +0100

    Update xml2json.py

--- python_scripts/xml2json.py
+++ python_scripts/xml2json.py
@@ -1,8 +1,7 @@
 import xmltodict,json,os
 
 folderpath = "dataset/"
-inputname = "dataset/book-db.xml"
-outputname ="dataset/book-db.json"
+
 for filename in os.listdir(folderpath):
     xml = open(folderpath+filename,"rb")
     f = open(filename.replace("xml","json"),"w")
