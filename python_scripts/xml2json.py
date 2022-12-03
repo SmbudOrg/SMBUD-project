cf7e1f2cdefe9031a5c49e2c6fc36fac731c35ed
Pietro Caforio <34812011+Pete2000@users.noreply.github.com>
2022-12-03 15:40:23 +0100

    Update xml2json.py

--- python_scripts/xml2json.py
+++ python_scripts/xml2json.py
@@ -6,5 +6,4 @@ for filename in os.listdir(folderpath):
     xml = open(folderpath+filename,"rb")
     f = open(filename.replace("xml","json"),"w")
     f.write(json.dumps(xmltodict.parse(xml), indent=4))
-
-
+#!Disclaimer: non importare su spark i file direttamente convertiti, rimuovi prima dblp e "articles" e lascia solo le parentesi quadre 
