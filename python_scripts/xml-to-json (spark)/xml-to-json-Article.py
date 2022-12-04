d9acc6fba0a9b0b0cf71557029290d91955c05bb
ChristianAndreoli00 <115942228+ChristianAndreoli00@users.noreply.github.com>
2022-12-04 16:53:38 +0100

    edit

---
+++ python_scripts/xml-to-json (spark)/xml-to-json-Article.py
@@ -0,0 +1,63 @@
+import xml.etree.ElementTree as ET
+import json
+
+inputfile = "Datasets (xml)/article-db.xml"
+outputfile = "Datasets (json)/article-db.json"
+
+data_out = []
+
+#author, editor e series hanno una key e quindi sono presenti separatamente nello script
+#ALTRI SOTTOELEMENTI:
+#sottoelementi che sono stringhe semplici
+subelements = ["title", "publisher", "journal"]
+#sottoelementi che sono array di stringhe
+arraysubelements = ["keyword", "ee", "cite"]
+#sottoelementi che sono int semplici
+intsubelements = ["year", "citations", "pages", "volume"]
+
+with open(inputfile, "r") as f_in:
+    input_tree = ET.parse(f_in)
+    print("Input tree built")
+    input_root = input_tree.getroot()
+
+    for element in list(input_root):
+        pub = {}
+        pub["key"] = element.get("key")
+
+        authors = element.findall("author")
+        if (authors is not None and len(authors)>0):
+            pub["author"] = []
+            for a in authors:
+                json_a = {"orcid": a.get("orcid"), "name": a.text}
+                pub["author"].append(json_a)
+        
+        editors = element.findall("editor")
+        if (editors is not None and len(editors)>0):
+            pub["editor"] = []
+            for e in editors:
+                json_e = {"orcid": e.get("orcid"), "name": e.text}
+                pub["editor"].append(json_e)
+        
+        series = element.find("series")
+        if (series is not None):
+            pub["series"] = {"href": series.get("href"), "title": series.text}
+        
+        for s in subelements:
+            if (element.find(s) is not None):
+                pub[s] = element.find(s).text
+        
+        for asubelement in arraysubelements:
+            json_as = element.findall(asubelement)
+            if (json_as is not None and len(json_as)>0):
+                pub[asubelement] = [i.text for i in json_as]
+        
+        for intsubel in intsubelements:
+            if (element.find(intsubel) is not None):
+                pub[intsubel] = int(element.find(intsubel).text)
+
+        data_out.append(pub)
+
+print("Parsing ended")
+
+with open(outputfile, "w") as f_out:
+    json.dump(data_out, f_out, indent=4)
